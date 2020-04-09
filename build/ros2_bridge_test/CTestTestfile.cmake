0152e5dc0d11d095f11fd37b7034f923f373e2a5
amartyadash <amartya@iitk.ac.in>
2020-04-09 16:19:00 +0530

    Reverted minor accident

--- build/ros2_bridge_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amartya/dev_ws/src/ros2_bridge_test
-# Build directory: /home/amartya/dev_ws/src/ros2_bridge_test/build/ros2_bridge_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
